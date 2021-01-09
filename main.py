4788dc905ad2477a977dda71dd82dd685ee9efe0
carolaltimas <55726229+carolaltimas@users.noreply.github.com>
2021-01-09 13:17:58 -0500

    Intial tructure of Game

---
+++ main.py
@@ -0,0 +1,28 @@
+## Project: Money Does Grow On Trees ##
+## Authors: Aranya Sutharsan, Rinoa Malapaya Noshin Rahman, Carol Altimas ##
+
+import time  # Imports a module to add a pause
+
+# User Responses
+answer_A = ["A", "a"]
+answer_B = ["B", "b"]
+answer_C = ["C", "c"]
+yes = ["Y", "y", "yes"]
+no = ["N", "n", "no"]
+
+# Variables for the game
+green_points = 0
+chequings_acc = 1000
+savings_acc = 0
+money = 0
+
+
+## Introduction ##
+
+
+## Morning ##
+
+
+## Afternoon ##
+
+## Night ##
